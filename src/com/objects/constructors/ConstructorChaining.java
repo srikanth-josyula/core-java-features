c7298975c5cd01246da84a02a103f1841adae448
Srikanth Josyula <44222500+srikanth-josyula@users.noreply.github.com>
2024-08-27 14:34:56 -0400

    Add files via upload

---
+++ src/com/objects/constructors/ConstructorChaining.java
@@ -0,0 +1,57 @@
+package com.objects.constructors;
+
+public class ConstructorChaining extends ConstructorChainingParent {
+
+	private String msgChild;
+	
+	public ConstructorChaining() {
+		this("Child"); // Calls parameterized constructor of the same class (constructor chaining)
+		System.out.println("Child no-argument constructor");
+	}
+	
+	// Parameterized constructor
+    public ConstructorChaining(String parentValue, String childValue) {
+        super(parentValue); // Calls parameterized constructor of the parent class (constructor chaining with super)
+        this.msgChild = childValue;
+        System.out.println("Child parameterized constructor with String value = " + this.msgChild);
+    }
+    
+    public ConstructorChaining(String value) {
+    	this.msgChild = value;
+    	System.out.println("Child parameterized constructor with String value = " + this.msgChild);
+	}
+
+	public static void main(String[] args) {
+    	ConstructorChaining obj1 = new ConstructorChaining(); // Calls no-argument constructor of Child
+        System.out.println();
+        ConstructorChaining obj2 = new ConstructorChaining("Parent","Child"); // Calls parameterized constructor of Child with one argument
+       
+    }
+}
+
+class ConstructorChainingParent {
+
+	private String msg;
+
+	public ConstructorChainingParent() {
+		this("Parent"); // Calls parameterized constructor of the same class (constructor chaining)
+		System.out.println("Parent no-argument constructor");
+	}
+
+	// Parameterized constructor
+	public ConstructorChainingParent(String value) {
+		this.msg = value;
+		System.out.println("Parent parameterized constructor with String = " + this.msg);
+	}
+}
+
+/**
+ * Parent parameterized constructor with String = Parent
+ * Parent no-argument constructor
+ * Child parameterized constructor with String value = Child
+ * Child no-argument constructor
+ * 
+ * Parent parameterized constructor with String = Parent
+ * Child parameterized constructor with String value = Child
+ *
+ **/
